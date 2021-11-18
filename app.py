ecb7e5ffe6401775db42f8ebb9ce1d6efc507fbc
Henri Malinen <henri.malinen@sofigate.com>
2021-11-18 18:54:08 +0200

    juukeli

--- app.py
+++ app.py
@@ -10,59 +10,8 @@ import ssl
 import sqlalchemy
 
 def get_db_connection():
-    db_config = {
-        # [START cloud_sql_postgres_sqlalchemy_limit]
-        # Pool size is the maximum number of permanent connections to keep.
-        "pool_size": 5,
-        # Temporarily exceeds the set pool_size if no connections are available.
-        "max_overflow": 2,
-        # The total number of concurrent connections for your application will be
-        # a total of pool_size and max_overflow.
-        # [END cloud_sql_postgres_sqlalchemy_limit]
-
-        # [START cloud_sql_postgres_sqlalchemy_backoff]
-        # SQLAlchemy automatically uses delays between failed connection attempts,
-        # but provides no arguments for configuration.
-        # [END cloud_sql_postgres_sqlalchemy_backoff]
-
-        # [START cloud_sql_postgres_sqlalchemy_timeout]
-        # 'pool_timeout' is the maximum number of seconds to wait when retrieving a
-        # new connection from the pool. After the specified amount of time, an
-        # exception will be thrown.
-        "pool_timeout": 30,  # 30 seconds
-        # [END cloud_sql_postgres_sqlalchemy_timeout]
-
-        # [START cloud_sql_postgres_sqlalchemy_lifetime]
-        # 'pool_recycle' is the maximum number of seconds a connection can persist.
-        # Connections that live longer than the specified amount of time will be
-        # reestablished
-        "pool_recycle": 1800,  # 30 minutes
-        # [END cloud_sql_postgres_sqlalchemy_lifetime]
-    }
-
-
-    return init_tcp_connection_engine(db_config)
-
-
-def init_tcp_connection_engine(db_config):
-    pool = sqlalchemy.create_engine(
-        # Equivalent URL:
-        # postgresql+pg8000://<db_user>:<db_pass>@<db_host>:<db_port>/<db_name>
-        sqlalchemy.engine.url.URL.create(
-            drivername="postgresql+pg8000",
-            username="keijo",  # e.g. "my-database-user"
-            password="keijo",  # e.g. "my-database-password"
-            host="127.0.0.1",  # e.g. "127.0.0.1"
-            port=5432,  # e.g. 5432
-            database="keijo"  # e.g. "my-database-name"
-        ),
-        **db_config
-    )
-    # [END cloud_sql_postgres_sqlalchemy_create_tcp]
-    pool.dialect.description_encoding = None
-    return pool
-
-#
+    con = psycopg2.connect(**config.config())
+    return con
 
 
 def get_post(post_id):
